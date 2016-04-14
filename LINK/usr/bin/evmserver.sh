d51858ac1eb7e4d58c3610d9f570134cea0bb777
Juraci Paixão Kröhling <juraci@kroehling.de>
2016-04-14 11:44:04 +0200

    Initial commit, based on manageiq-appliance

---
+++ LINK/usr/bin/evmserver.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local:/usr/local/sbin:/usr/local/bin
+BASEDIR="/var/www/miq/vmdb"
+RETVAL=0
+cd $BASEDIR
+
+# Load default environment and variables
+[[ -s "/etc/default/evm" ]] && source "/etc/default/evm"
+
+[[ -x "${APPLIANCE_SOURCE_DIRECTORY}/initialize_appliance.sh" ]] && ${APPLIANCE_SOURCE_DIRECTORY}/initialize_appliance.sh
+
+start() {
+  rake evm:start
+  RETVAL=$?
+  return $RETVAL
+}
+
+stop() {
+  rake evm:stop
+  RETVAL=$?
+  return $RETVAL
+}
+
+kill() {
+  rake evm:kill
+  RETVAL=$?
+  return $RETVAL
+}
+
+restart() {
+  rake evm:restart
+  RETVAL=$?
+  return $RETVAL
+}
+
+status() {
+  rake evm:status
+  RETVAL=$?
+  return $RETVAL
+}
+
+update_start() {
+  rake evm:update_start >> /var/www/miq/vmdb/log/evm.log 2>&1
+  RETVAL=$?
+  return $RETVAL
+}
+
+update_stop() {
+  rake evm:update_stop >> /var/www/miq/vmdb/log/evm.log 2>&1
+  RETVAL=$?
+  return $RETVAL
+}
+
+# See how we were called.
+case "$1" in
+  start)
+  start
+  ;;
+  stop)
+  stop
+  ;;
+  kill)
+  kill
+  ;;
+  status)
+  status
+  ;;
+  restart)
+  restart
+  ;;
+  update_start)
+  update_start
+  ;;
+  update_stop)
+  update_stop
+  ;;
+  *)
+  echo $"Usage: $prog {start|stop|restart|status}"
+  exit 1
+esac
+
+exit $RETVAL
---
+++ hawkular-setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+[[ -s /etc/default/evm ]] && source /etc/default/evm
+
+pushd /var/www/miq/vmdb
+  RAILS_ENV=production rake evm:compile_assets
+  rake evm:compile_sti_loader
+popd
+
+# Self Service UI
+pushd /opt/manageiq/manageiq-ui-self_service
+  npm install gulp bower -g
+  npm install
+  bower -F --allow-root install
+  gulp build
+popd
+
+# httpd needs to connect to backend workers on :3000 and :4000
+setsebool -P httpd_can_network_connect on
+
+# apply SELinux policies needed by the appliance
+[[ -x /usr/bin/miqselinux.sh ]] && /usr/bin/miqselinux.sh
+
+# backup the default ssl.conf
+mv /etc/httpd/conf.d/ssl.conf{,.orig}
+
+# https://bugzilla.redhat.com/show_bug.cgi?id=1020042
+cat <<'EOF' > /etc/httpd/conf.d/ssl.conf
+# This file intentionally left blank.  CFME maintains its own SSL
+# configuration.  The presence of this file prevents the version
+# supplied by mod_ssl from being installed when the mod_ssl package is
+# upgraded.
+EOF
+
+/usr/sbin/semanage fcontext -a -t httpd_log_t "/var/www/miq/vmdb/log(/.*)?"
+/usr/sbin/semanage fcontext -a -t cert_t "/var/www/miq/vmdb/certs(/.*)?"
+/usr/sbin/semanage fcontext -a -t logrotate_exec_t ${APPLIANCE_SOURCE_DIRECTORY}/logrotate_free_space_check.sh
+
+[ -x /sbin/restorecon ] && /sbin/restorecon -R -v /var/www/miq/vmdb/log
+[ -x /sbin/restorecon ] && /sbin/restorecon -R -v /etc/sysconfig
+[ -x /sbin/restorecon ] && /sbin/restorecon -R -v /var/www/miq/vmdb/certs
+[ -x /sbin/restorecon ] && /sbin/restorecon -R -v ${APPLIANCE_SOURCE_DIRECTORY}/logrotate_free_space_check.sh
+
+# relabel the pg_log directory in postgresql datadir, but defer restorecon
+# until after the database is initialized during firstboot configuration
+/usr/sbin/semanage fcontext -a -t var_log_t "${APPLIANCE_PG_DATA}/pg_log(/.*)?"
+
+# setup label for postgres client certs, but relabel after dir is created
+/usr/sbin/semanage fcontext -a -t cert_t "/root/.postgresql(/.*)?"
+# will remove this once app is no longer running as root
+/usr/sbin/semanage fcontext -a -t user_home_dir_t "/root(/)?"
+/sbin/restorecon /root
